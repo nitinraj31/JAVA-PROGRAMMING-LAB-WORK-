72bc85c907a703202c4afc707204e5b35b3aefba
NITIN RAJ <code.gecwc@gmail.com>
2026-01-31 10:45:03 +0530

    31-01-2026

---
+++ 31-01-2025/index.java
@@ -0,0 +1,13 @@
+
+
+public class index {
+  //applicable for voting or not 
+  public static void main(String[] args) {
+    int age = 20;
+    if(age>18){
+        System.out.println("you are eligible for voting ");
+    } else {
+        System.out.println("you are not eligible for voting ");
+    }
+  }
+}
