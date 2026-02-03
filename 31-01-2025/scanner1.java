d1b6d2238c1b1cd9a5dc8bb92a5c798955c3eb3a
NITIN RAJ <code.gecwc@gmail.com>
2026-02-03 17:34:10 +0530

    updated

---
+++ 31-01-2025/scanner1.java
@@ -0,0 +1,12 @@
+package 31-01-2025;
+
+public class scanner1 {
+    public static void main(String[] args) {
+        int n = 10;
+        if(n % 2 == 0){
+            System.out.println(n + " is an even number.");
+        } else {
+            System.out.println(n + " is an odd number.");
+        }
+    }
+}
